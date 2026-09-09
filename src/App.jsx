abcc440824764195b65eae03bf90bcd35eb01059
ridhakurfi <ridhakurfi@gmail.com>
2026-09-09 12:40:03 +0700

    22

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
           <h1>Daftar Gelar</h1>
 
           <p className="subtitle">
-            Gelar Guci dan para pemegangnya
+            Gelar Kampung Guci dan para pemegangnya
           </p>
         </div>
 
